f15ad0c10a467438a189a62e99bda2f215cb6985
Kristian Rosland <kristian.rosland@gmail.com>
2018-05-06 13:36:17 +0200

    Add new soundgame-component

---
+++ SoundGame.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+
+export default class SoundGame extends React.Component {
+
+  render() {
+    return (
+        <View style={styles.container}>
+            
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+});
